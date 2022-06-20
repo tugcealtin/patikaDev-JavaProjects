0a228389f4341c3e874fb7c54e60c378f8ee77ab
Tuğçe Altın <100563673+tugcealtin@users.noreply.github.com>
2022-06-20 17:00:59 +0300

    Update Main.java

--- mukemmelSayi/src/Main.java
+++ mukemmelSayi/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         do {
             toplam=0;
             Scanner scan = new Scanner(System.in);
-            System.out.print("Sayi ");
+            System.out.print("Sayi= ");
             sayi = scan.nextInt();
             for (int i = 1; i < sayi; i++) {
                 if (sayi % i == 0) {
@@ -15,9 +15,9 @@ public class Main {
                 }
 
             }if(sayi==toplam){
-                System.out.println(sayi+" sayisi mukkemmel sayidir");
+                System.out.println(sayi+" sayisi mukemmel sayidir");
             }else{
-                System.out.println(sayi+" sayisi mukkemmel degil");
+                System.out.println(sayi+" sayisi mukemmel degil");
             }
             System.out.println("Sorgu tekrar basliyor");
         } while (true);
