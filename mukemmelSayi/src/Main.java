39614e1f92bd7026f172cf05f7b136ed58ed712d
Tuğçe Altın <100563673+tugcealtin@users.noreply.github.com>
2022-06-20 16:59:07 +0300

    mukkemel sayıyı bulan program eklendi
    do-while  ile mukemmel sayiyi bulan program

---
+++ mukemmelSayi/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int sayi;
+        int toplam ;
+        do {
+            toplam=0;
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Sayi ");
+            sayi = scan.nextInt();
+            for (int i = 1; i < sayi; i++) {
+                if (sayi % i == 0) {
+                    toplam = toplam + i;
+                }
+
+            }if(sayi==toplam){
+                System.out.println(sayi+" sayisi mukkemmel sayidir");
+            }else{
+                System.out.println(sayi+" sayisi mukkemmel degil");
+            }
+            System.out.println("Sorgu tekrar basliyor");
+        } while (true);
+    }
+}
